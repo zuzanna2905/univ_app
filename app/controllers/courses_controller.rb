217421fa52c8cff458428d6ce655695311fd9198
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-06-14 13:39:46 +0200

    add materialize and set navbar

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,4 +3,6 @@ class CoursesController < ApplicationController
     def index
     end
     
+    def new
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'courses#index'
+  get 'courses/new', to: 'courses#new'
+  get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
