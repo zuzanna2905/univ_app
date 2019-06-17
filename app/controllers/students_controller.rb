4e00dd821e44f4db3dd6598aa47191ffdead76e9
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-06-17 14:42:28 +0200

    add password to form

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -40,6 +40,6 @@ class StudentsController < ApplicationController
     end
 
     def student_parms
-        params.require(:student).permit(:name, :email)
+        params.require(:student).permit(:name, :email, :password, :password_confirmation)
     end
 end
