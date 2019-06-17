b6ca15038a7c1aea924ecfb006590659e5ea1511
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-06-17 15:21:31 +0200

    add sessions

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -4,9 +4,21 @@ class LoginsController < ApplicationController
     end
 
     def create
+        @student = Student.find_by(email: params[:logins][:email].downcase)
+        if @student && @student.authenticate(params[:logins][:password])
+            session[:student_id] = @student.id
+            flash.now[:success] = 'You have successfully logged in'
+            redirect_to student_path(@student)
+        else
+            flash.now[:success] = 'Something was wrong with your login information'
+            render 'new'
+        end
     end
 
     def destroy
+        session[:student_id] = nil
+        flash[:success] = 'You have successfully logged out'
+        redirect_to root_path
     end
 
 end
